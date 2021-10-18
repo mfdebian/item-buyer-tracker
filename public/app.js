9366c3cfe4c7be68c43230b3eda682d2cb901761
daydream <alfredo.arosteguy@gmail.com>
2021-10-17 23:07:49 -0300

    WIP: Search function - Now results display in DOM

--- public/app.js
+++ public/app.js
@@ -1,36 +1,40 @@
 import { firebaseInit } from './firebase.js'
 
 firebaseInit();
+const rootDiv = document.getElementById("root");
+
 
 const search = (itemsArray) => {
-  let rootDiv = document.getElementById("root");
   
   let searchBar = document.createElement("input");
   searchBar.type = "text";
   searchBar.placeholder = "Type your item here"
 
   let searchResultsDiv = document.createElement("p");
-  
-  searchBar.addEventListener("keyup", (event) => {
-    if(event.key !== "Backspace" && event.key !== "Delete" && event.key !== "Tab" && event.key !== "Control" && event.key !== "Shift") {
-      let searchInput = document.createTextNode(event.key);
 
-      let searchResults = itemsArray.filter(item =>
-        Object.keys(item).some(key => item.name.toLowerCase().includes(searchBar.value.toLowerCase())));
-      
-      console.log(searchResults);
+  let searchButton = document.createElement("button");
+  searchButton.textContent = "Search"
 
-      searchResultsDiv.appendChild(searchInput);
-    } else {
-      if(searchResultsDiv.lastChild) {
-        searchResultsDiv.removeChild(searchResultsDiv.lastChild);
-      }
-    }
+  searchButton.addEventListener("click", () => {
+    searchResultsDiv.textContent = "";
+    let searchInput = searchBar.value.toLowerCase();
+    let searchResults = getItems(itemsArray, searchInput);
+    printItemsInDom(searchResultsDiv, searchResults);
+
+
   }, false);
 
   rootDiv.appendChild(searchBar);
+  rootDiv.appendChild(searchButton);
   rootDiv.appendChild(searchResultsDiv);
 }
+
+ const getItems = (itemsArray, searchInput) => {
+  let searchResults = itemsArray.filter(item =>
+    Object.keys(item).some(key => item.name.toLowerCase().includes(searchInput)));
+
+   return searchResults;
+ }
 
 
 fetch('./data/items.json')
@@ -55,36 +59,35 @@ const processData = (itemsArray) => {
       return (item.name > nextItem.name) ? 1 : ((nextItem.name > item.name) ? -1 : 0);
     });
 
-    printItemsInDom(itemsArrayByBuyer);
+    printItemsInDom(rootDiv, itemsArrayByBuyer);
   });
 }
 
-const printItemsInDom = (items) => {
-  let rootDiv = document.getElementById("root");
+const printItemsInDom = (container, items) => {
   
   items.forEach((item, index) => {
     if(index === 0) {
-      rootDiv.appendChild(document.createElement("hr"));
+      container.appendChild(document.createElement("hr"));
 
       let buyer = document.createElement("span");
       buyer.textContent = item.buyer;
       buyer.setAttribute("class", "buyer");
 
-      rootDiv.appendChild(buyer);
-      rootDiv.appendChild(document.createElement("br"));
+      container.appendChild(buyer);
+      container.appendChild(document.createElement("br"));
     }
     
     let name = document.createElement("span");
     name.setAttribute("class", "name");
     name.textContent = item.name;
-    rootDiv.appendChild(name);
+    container.appendChild(name);
     
     let value = document.createElement("span");
     value.setAttribute("class", "value");
     value.textContent = item.value + "k";
-    rootDiv.appendChild(value);
+    container.appendChild(value);
 
-    rootDiv.appendChild(document.createElement("br"));
+    container.appendChild(document.createElement("br"));
   });
 
 }
