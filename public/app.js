5ec902e92d9ffdd1fb4fcc700948a36eda551ef2
daydream <alfredo.arosteguy@gmail.com>
2022-06-03 21:15:29 -0400

    Refactor code: got rid of getItems function and now processData returns an items object

--- public/app.js
+++ public/app.js
@@ -3,8 +3,24 @@ import { firebaseInit } from './firebase.js'
 firebaseInit();
 const rootDiv = document.getElementById("root");
 
+fetch('./data/items.json')
+  .then(response => response.json())
+  .then(data => {
+    searchSection(data.items);
+    let items = processData(data.items);
+    printItemsInDom(rootDiv, items);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
-const search = (itemsArray) => {
+
+/* 
+  searchSection() creates the search bar, the search button
+  and the search results div. It attaches events that'll trigger
+  the search function.
+*/
+const searchSection = (itemsArray) => {
   let searchResultsDiv = document.createElement("div");
 
   let searchBar = document.createElement("input");
@@ -17,86 +33,95 @@ const search = (itemsArray) => {
   rootDiv.appendChild(searchBar);
   rootDiv.appendChild(searchButton);
   rootDiv.appendChild(searchResultsDiv);
-
-  const getSearchResultsAndCallprocessData = () => {
-    searchResultsDiv.textContent = "";
-    searchResultsDiv.setAttribute("class", "searchResultsDiv");
-    let searchInput = searchBar.value.toLowerCase();
-    let searchResults = getItems(itemsArray, searchInput);
-    processData(searchResultsDiv, searchResults);
-  }
   
   searchBar.addEventListener("keydown", (event) => {
-    if(event.key==="Enter") {
-      getSearchResultsAndCallprocessData();
+    if(event.key === "Enter") {
+      search();
     }
   }, false);
 
   searchButton.addEventListener("click", () => {
-    getSearchResultsAndCallprocessData();
+    search();
   }, false);
 
+  /*
+    search() filters the items by the user's input.
+    Then calls the function to process the data and 
+    lastly the function to display it.
+  */
+  const search = () => {
+    searchResultsDiv.textContent = "";
+    searchResultsDiv.setAttribute("class", "searchResultsDiv");
+    let searchInput = searchBar.value.toLowerCase();
+    
+    let searchResults = itemsArray.filter(item =>
+      Object.keys(item).some(() => item.name.toLowerCase().includes(searchInput)));
+
+    let items = processData(searchResults);
+    printItemsInDom(searchResultsDiv, items);
+  }
 }
 
-
-const getItems = (itemsArray, searchInput) => {
-let searchResults = itemsArray.filter(item =>
-  Object.keys(item).some(key => item.name.toLowerCase().includes(searchInput)));
-
-  return searchResults;
-}
-
-
-fetch('./data/items.json')
-  .then(response => response.json())
-  .then(data => {
-    search(data.items);
-    processData(rootDiv, data.items);
-  })
-  .catch(err => {
-    console.log(err);
-  });
-
-
-const processData = (_, itemsArray) => {
+/*
+  processData() will receive an items' array and return
+  an items object in which every key will be a buyer and
+  it's corresponding items will be the values.
+*/
+const processData = (itemsArray) => {
   let uniqueBuyersInData = [...new Set(itemsArray.map(item => item.buyer))];
   uniqueBuyersInData = uniqueBuyersInData.sort();
+  let itemsObject = {};
 
   uniqueBuyersInData.forEach(buyer => {
-
+    
     let itemsArrayByBuyer = itemsArray.filter(item => item.buyer === buyer);
+
     itemsArrayByBuyer = itemsArrayByBuyer.sort((item, nextItem) => {
       return (item.name > nextItem.name) ? 1 : ((nextItem.name > item.name) ? -1 : 0);
     });
+    
+    itemsObject[buyer] = itemsArrayByBuyer;
 
-    printItemsInDom(_, itemsArrayByBuyer);
   });
+
+  return itemsObject;
+
 }
 
+/*
+  printItemsInDom() will receive the container in which
+  to display data and the item's object to display
+  data by buyer.
+*/
+const printItemsInDom = (container, itemsObject) => {
 
-const printItemsInDom = (container, items) => {
-  items.forEach((item, index) => {
-    if(index === 0) {
-      container.appendChild(document.createElement("hr"));
+  let buyers = Object.keys(itemsObject);
+  buyers = buyers.sort();
 
-      let buyer = document.createElement("span");
-      buyer.textContent = item.buyer;
-      buyer.setAttribute("class", "buyer");
-
-      container.appendChild(buyer);
+  buyers.forEach(buyer => {
+    itemsObject[buyer].forEach((item, index) => {
+      if(index === 0) {
+        container.appendChild(document.createElement("hr"));
+  
+        let buyer = document.createElement("span");
+        buyer.textContent = item.buyer;
+        buyer.setAttribute("class", "buyer");
+  
+        container.appendChild(buyer);
+        container.appendChild(document.createElement("br"));
+      }
+      
+      let name = document.createElement("span");
+      name.setAttribute("class", "name");
+      name.textContent = item.name;
+      container.appendChild(name);
+      
+      let value = document.createElement("span");
+      value.setAttribute("class", "value");
+      value.textContent = item.value + "k";
+      container.appendChild(value);
+  
       container.appendChild(document.createElement("br"));
-    }
-    
-    let name = document.createElement("span");
-    name.setAttribute("class", "name");
-    name.textContent = item.name;
-    container.appendChild(name);
-    
-    let value = document.createElement("span");
-    value.setAttribute("class", "value");
-    value.textContent = item.value + "k";
-    container.appendChild(value);
-
-    container.appendChild(document.createElement("br"));
+    });
   });
 }
